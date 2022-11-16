049717ea2145b803f60e53d7706027423560f13d
sajid <s.rahman@einhundert-energie.de>
2022-11-16 11:52:28 +0100

    almost everything together ;)

--- demo/classes.py
+++ demo/classes.py
@@ -1,45 +1,47 @@
 import random
+import time
 
 
 class CustomMeta(type):
     def __call__(cls, *args, **kwargs):
-        print(f"I am a blocker of {cls.__name__}. I won't let you proceed fro here")
+        print(f"I am inside the CustomMeta.__call__ method.")
+        print(f"I am going to run object.__new__ method. This will create a new object of type {cls}")
         obj = object.__new__(cls)
+        print(f"We have created the object and allocated memory for it. Here is the object {obj}")
+
+        print("We are going to attach an attribute called `creation_timestamp`.")
+        setattr(obj, "creation_timestamp", time.time_ns())
+        print(f"Here is the value of {obj.creation_timestamp}")
+        print(f"Now we will initiate the object {obj}")
         obj.__init__(*args, **kwargs)
+        print(f"We are done with metafying. Now we are handing you over the object. Wish you good luck with {obj}")
         return obj
 
 
-class Root(metaclass=CustomMeta):
+class SpecialClass(metaclass=CustomMeta):
 
-    def __init__(self, name:str):
-        self.name = name
-
-    def greet(self):
-        print("Hi!")
-
-
-class Sub1(Root):
-
-    def greet(self):
-        print("You are in Sub1")
-
-    def dummy(self):
-        print("dummy of Sub1")
-
-
-class Sub2:
-
-    def foo(self):
-        print("You are in Sub1")
-
-    def bar(self):
-        print("dummy of Sub1")
-
-
-class Child(Sub1, Sub2):
     def __init__(self, name: str):
         self.name = name
 
 
-def factory(cls: type, *args) -> object:
-    return object.__new__(cls, *args)
+class NormalClass:
+
+    def __init__(self, name: str, age: int = 20):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+def timestamped_object_factory(cls: type, *args, **kwargs) -> object:
+    obj = object.__new__(cls, *args, **kwargs)
+    setattr(obj, "creation_timestamp", time.time_ns())
+    return obj
+
+
+def better_timestamped_object_factory(cls: type, *args, **kwargs) -> object:
+    obj = object.__new__(cls)
+    obj.__init__(*args, **kwargs)
+    if getattr(obj, "creation_timestamp", None):
+        setattr(obj, "creation_timestamp", time.time_ns())
+    return obj
--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 A small project to demonstrate the workflow of python's magic/dunder methods in combination with Meta class.
 """
 
-from demo.classes import *
+from demo.classes import SpecialClass, NormalClass, timestamped_object_factory, better_timestamped_object_factory
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,10 +12,23 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    root = Root(name="Sajid")
-    try:
-        name = root.name
-        print(name)
-    except AttributeError as e:
-        print(e)
+    timestamped_obj = SpecialClass(name="Max")
+    print(f"The timestamped_obj object was created at {getattr(timestamped_obj, 'creation_timestamp', None)}")
 
+    normal_obj = NormalClass(name="Michael")
+    print(f"The normal_obj object was created at {getattr(normal_obj, 'creation_timestamp', 'Unknown time')}")
+
+    factory_manufactured_normal_obj = timestamped_object_factory(NormalClass)
+    print(f"The normal_obj object was created at:"
+          f"# {getattr(factory_manufactured_normal_obj, 'creation_timestamp', 'Unknown time')}")
+
+    # Since the __init__  of factory_manufactured_normal_obj was not called we can't access the name attribute.
+    try:
+        factory_manufactured_normal_obj.get_name()
+    except AttributeError:
+        print(f" We are handling AttributeError exception because"
+              f" factory_manufactured_normal_obj's get_name() could not access the name attribute.")
+
+    another_factory_manufactured_normal_obj = better_timestamped_object_factory(NormalClass, "Empowered", age=55)
+    print(f" We successfully accessed the get_name of NormalClass object. Here is it's name:"
+          f" {another_factory_manufactured_normal_obj.get_name()}")
